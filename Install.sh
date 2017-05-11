952d7689b3c20d61f14184791108f23b65d9b6ea
vascofazza <fscozzafava@imacdifederico.lan>
2017-05-12 00:21:03 +0200

    fix

--- Install.sh
+++ Install.sh
@@ -9,7 +9,7 @@ sudo apt-get --assume-yes install python-smbus
 sudo git clone https://github.com/vascofazza/gbzattinymonitor.git
 sudo chmod 755 /home/pi/gbzattinymonitor/Pngview/pngview
 # Insert battery script into rc.local before final 'exit 0'
-if ! grep -Fxq "python \/home\/pi\/gbzattinymonitor\/monitor.py" /etc/rc.local
+if ! grep -Fxq "python /home/pi/gbzattinymonitor/monitor.py &" /etc/rc.local
 then
 sed -i "s/^exit 0/python \/home\/pi\/gbzattinymonitor\/monitor.py \&\\nexit 0/g" /etc/rc.local >/dev/null
 fi
