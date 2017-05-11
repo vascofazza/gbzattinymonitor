468efb7575cd6836e10b3dcb133ef4bd9bd5cd3b
vascofazza <fscozzafava@imacdifederico.lan>
2017-05-12 00:34:56 +0200

    fix install

--- Install.sh
+++ Install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-cd ~
 sudo apt-get update
 sudo apt-get --assume-yes install libpng12-dev
 sudo apt-get --assume-yes install python-gpiozero
 sudo apt-get --assume-yes install python-pkg-resources
 sudo apt-get --assume-yes install python-smbus
-sudo git clone https://github.com/vascofazza/gbzattinymonitor.git
-sudo chmod 755 /home/pi/gbzattinymonitor/Pngview/pngview
+git clone https://github.com/vascofazza/gbzattinymonitor.git /home/pi/gbzattinymonitor
+chmod 755 /home/pi/gbzattinymonitor/Pngview/pngview
 # Insert battery script into rc.local before final 'exit 0'
 if ! grep -Fxq "python /home/pi/gbzattinymonitor/monitor.py &" /etc/rc.local
 then
